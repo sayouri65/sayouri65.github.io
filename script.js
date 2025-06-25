148504e08a089a7a77f208ba1469f4243a92ead6
Patricia (Sayouri) <106440476+sayouri65@users.noreply.github.com>
2025-06-25 16:26:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 if (performance.navigation.type === 1) {
     window.location.href = "index.html";
+    style.setAttribute("href", "style.css");
+}
+
+var style = document.getElementById("stylesheet");
+function showSection(section) {
+    const sections = [
+        { name: "aboutMe", block: "aboutMeBlock", button: "aboutMeButton" },
+        { name: "projects", block: "projectsBlocks", button: "projectsButton" },
+        { name: "contact", block: "contactBlock", button: "contactButton" },
+    ];
+
+    sections.forEach(s => {
+        const block = document.getElementById(s.block);
+        const button = document.getElementById(s.button);
+        const img1 = document.getElementById("imgsubp");
+        const img2 = document.getElementById("imgmainp");
+        if (s.name === section) {
+            block.style.display = "block";
+            button.className = "btn-sellected";
+            img1.style.display = "none"; img2.style.display = "block";
+        } else {
+            block.style.display = "none";
+            button.className = "btn";
+        }
+    });
+    style.setAttribute("href", "style2.css");
 }
